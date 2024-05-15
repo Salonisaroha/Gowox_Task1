10a3bcf76ee0ec9cb73814251079b3b0d81f50fe
Saloni Saroha <salonisaroha5@gmail.com>
2024-05-15 19:24:49 +0530

    Gradient colors

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 class CustomColors {
   static LinearGradient bgGradient = const LinearGradient(colors: [
- Color(0xFF9C59E),
  Color(0xFF9C59FE),
+ Color(0xFF6F53FD),
   ]
  );
   
